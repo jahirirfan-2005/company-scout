ea930ee1e4f199408d14ba1f24a9c27b5ad6467d
jahirirfan-2005 <jahirirfan645@gmail.com>
2026-07-01 13:52:52 +0530

    build: ignore and untrack env, venv, local history, database, and pycache files

--- .history/backend/companies/urls_20260630175919.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CompanySearchView, CompanyListView
-
-urlpatterns = [
-    path('search/', CompanySearchView.as_view(), name='company-search'),
-    path('', CompanyListView.as_view(), name='company-list'),
-]
-x
